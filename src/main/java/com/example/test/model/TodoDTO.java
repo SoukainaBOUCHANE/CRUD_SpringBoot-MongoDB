becd1a6cf25d1c22de6f103e8edb609b0a68731b
SoukainaBOUCHANE <96949957+SoukainaBOUCHANE@users.noreply.github.com>
2022-02-19 20:14:50 +0100

    Update TodoDTO.java

--- src/main/java/com/example/test/model/TodoDTO.java
+++ src/main/java/com/example/test/model/TodoDTO.java
@@ -12,10 +12,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Document(collection="todos")
 public class TodoDTO {
 	
